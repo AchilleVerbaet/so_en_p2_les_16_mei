eb7cf68be8deab15f1aa391229b547c4c6c6c1f2
AchilleVerbaet <achille.verbaet@student.kuleuven.be>
2025-05-23 13:32:39 +0200

    Update decaan_verkiezing.py

--- decaan_verkiezing.py
+++ decaan_verkiezing.py
@@ -42,5 +42,5 @@ lijst_kiezers = [
     DecaanKiezer("Broer", "Natuurkunde")]
 
 for kiezer in lijst_kiezers:
-    for kandidaat in lijst_van_kandidaten:
-        kiezer.stem(kandidaat)
+    kandidaatnummer = random.randint(0, len(lijst_van_kandidaten)-1)
+    kiezer.stem(lijst_van_kandidaten[kandidaatnummer])
